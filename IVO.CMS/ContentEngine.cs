a6c582af9b105e8b879bfc18d8cafa17ea0b7d72
James Dunne <james.jdunne@gmail.com>
2011-09-02 00:24:54 -0500

    Stubbed out documentation for intended schema and behavior of special XML processing instructions.

--- IVO.CMS/ContentEngine.cs
+++ IVO.CMS/ContentEngine.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Web;
 using System.Xml;
 using IVO.Definition.Models;
-using System.Web;
+using IVO.Definition.Repositories;
 
 namespace IVO.CMS
 {
@@ -14,9 +15,27 @@ namespace IVO.CMS
         private static readonly byte[] rootOpen = Encoding.UTF8.GetBytes("<" + rootElementName + ">");
         private static readonly byte[] rootClose = Encoding.UTF8.GetBytes("</" + rootElementName +">");
 
-        public HTMLFragment RenderBlob(Blob bl)
+        private ITreeRepository trrepo;
+        private IBlobRepository blrepo;
+
+        public ContentEngine(ITreeRepository trrepo, IBlobRepository blrepo)
+        {
+            this.trrepo = trrepo;
+            this.blrepo = blrepo;
+        }
+
+        public HTMLFragment RenderBlob(CanonicalizedAbsolutePath path, TreeID rootid, Blob bl)
         {
             // NOTE: I would much prefer to load in a Stream from the persistence store rather than a `byte[]`.
+            // It seems the only way to do this from a SqlDataReader is with its GetBytes() method. Furthermore,
+            // this would require the blob query to be executed with the CommandBehavior.SequentialAccess and
+            // the data would have to be streamed in while the SqlDataReader is open and passed to this method,
+            // which would be quite a tightly-integrated model. Perhaps the query class could be passed a lambda
+            // which then gets invoked during SqlDataReader parsing. That would call into this method so we could
+            // operate over blobs streamed directly from the database. Creating a new StreamedBlob type would be
+            // a good fit here too.
+            // Side note: The SqlDataReader's Stream abstractions are broken and only operate over in-memory
+            // data copies, thus defeating the purpose of streaming from the persistence store.
 
             // Create a string builder used to build the output polyglot HTML5 document fragment:
             StringBuilder sb = new StringBuilder(bl.Contents.Length);
@@ -50,8 +69,8 @@ namespace IVO.CMS
                         case XmlNodeType.Element:
                             switch (xr.LocalName)
                             {
-                                case "schedule":
-                                    processScheduleElement(xr, sb);
+                                case "scheduled":
+                                    processScheduledElement(xr, sb);
                                     break;
                                 case "audience":
                                     processAudienceElement(xr, sb);
@@ -93,7 +112,7 @@ namespace IVO.CMS
                             break;
 
                         case XmlNodeType.Whitespace:
-                            // NOTE: Whitespace strips out '\r' chars apparently.
+                            // NOTE: Whitespace node strips out '\r' chars apparently.
                             sb.Append(xr.Value);
                             break;
 
@@ -102,11 +121,26 @@ namespace IVO.CMS
                             sb.Append(HttpUtility.HtmlEncode(xr.Value));
                             break;
 
+                        case XmlNodeType.EntityReference:
+                            sb.Append(HttpUtility.HtmlEncode(xr.Value));
+                            break;
+
+                        case XmlNodeType.Comment:
+                            // FIXME: encode the comment text somehow? What rules?
+                            sb.AppendFormat("<!--{0}-->", xr.Value);
+                            break;
+
+                        case XmlNodeType.CDATA:
+                            throw new NotSupportedException("CDATA is not supported by this CMS.");
+
                         default:
                             throw new NotImplementedException();
                     }
                 } while (xr.Read());
             }
+
+            // Clear the reference to make GC's job a bit easier:
+            rootedContents = null;
 
             string result = sb.ToString();
             return new HTMLFragment(result);
@@ -114,16 +148,56 @@ namespace IVO.CMS
 
         private void processImportElement(XmlReader xr, StringBuilder sb)
         {
+            // Imports content directly from another blob, addressable by a relative path or an absolute path.
+            // Relative path is always relative to the current blob's absolute path.
+            // In the case of nested imports, relative paths are relative to the absolute path of the importee's parent blob.
+
+            // <import relative-path="../templates/main" />
+            // <import absolute-path="/templates/main" />
+
+            // Absolute paths are canonicalized. An exception will be thrown if the path contains too many '..' references that
+            // bring the canonicalized path above the root of the tree (which is impossible).
+
+            // Recursively call RenderBlob on the imported blob and include the rendered HTMLFragment into this rendering.
             throw new NotImplementedException();
         }
 
         private void processAudienceElement(XmlReader xr, StringBuilder sb)
         {
+            // <audience>
+            //   <!--
+            //     Order matters. Most specific targets come first; least specific targets go last.
+            //     Target attributes are user-defined. They must be valid XML attributes.
+            //     The custom attributes are collected into a Dictionary<string, string> and passed to
+            //     the "target evaluation provider" to evaluate if the target attributes indicate that
+            //     the content applies to the current user viewing the content.
+            //   -->
+            //   <target userType="Employee" department="Sales">
+            //     ... employee-targeted content here, specifically for Sales department ...
+            //   </target>
+            //   <target userType="Manager" />
+            //     ... manager-targeted content here, not specific to a department ...
+            //   <target userType="Employee" />
+            //   <else>
+            //     ... default content displayed if the above targets do not match ...
+            //   </else>
+            // </audience>
             throw new NotImplementedException();
         }
 
-        private void processScheduleElement(XmlReader xr, StringBuilder sb)
+        private void processScheduledElement(XmlReader xr, StringBuilder sb)
         {
+            // Specifies that content should be scheduled for the entire month of August
+            // and the entire month of October but NOT the month of September.
+            // 'from' is inclusive date/time.
+            // 'to'   is exclusive date/time.
+            // <scheduled>
+            //   <date-range from="2011-08-01 00:00 -0500" to="2011-09-01 00:00 -0500" />
+            //   <date-range from="2011-10-01 00:00 -0500" to="2011-11-01 00:00 -0500" />
+            //   <content>
+            //     ...
+            //   </content>
+            // </scheduled>
             throw new NotImplementedException();
         }
     }
--- TestCMS/ContentEngineTest.cs
+++ TestCMS/ContentEngineTest.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using IVO.Definition.Models;
 using IVO.CMS;
+using IVO.Definition.Repositories;
+using IVO.Implementation.SQL;
+using Asynq;
 
 namespace TestCMS
 {
@@ -16,13 +19,26 @@ namespace TestCMS
             Console.WriteLine(((string)fragment).Replace("\n", Environment.NewLine));
         }
 
+        private DataContext getDataContext()
+        {
+            return new DataContext(@"Data Source=.\SQLEXPRESS;Initial Catalog=IVO;Integrated Security=SSPI");
+        }
+
+        private ContentEngine getContentEngine()
+        {
+            var db = getDataContext();
+            ITreeRepository trrepo = new TreeRepository(db);
+            IBlobRepository blrepo = new BlobRepository(db);
+            return new ContentEngine(trrepo, blrepo);
+        }
+
         [TestMethod]
         public void TestRenderBlob()
         {
             Blob bl = new Blob.Builder(Encoding.UTF8.GetBytes(@"<a><b/><c/></a>
 <b></b>"));
-            var ce = new ContentEngine();
-            var frag = ce.RenderBlob(bl);
+            var ce = getContentEngine();
+            var frag = ce.RenderBlob(new CanonicalizedAbsolutePath("test"), new TreeID(), bl);
             output(frag);
         }
 
@@ -31,8 +47,8 @@ namespace TestCMS
         {
             Blob bl = new Blob.Builder(Encoding.UTF8.GetBytes(@"<a style=""color: &amp;too&quot;here&quot;"" href=""http://www.google.com/?a=1&amp;b=2"" target=""_blank""><b/><c/></a>
 <b class=""abc""></b>"));
-            var ce = new ContentEngine();
-            var frag = ce.RenderBlob(bl);
+            var ce = getContentEngine();
+            var frag = ce.RenderBlob(new CanonicalizedAbsolutePath("test"), new TreeID(), bl);
             output(frag);
         }
 
@@ -40,8 +56,8 @@ namespace TestCMS
         public void TestRenderBlobWithContent()
         {
             Blob bl = new Blob.Builder(Encoding.UTF8.GetBytes(@"<div><p>Some content &amp; stuff here. Maybe some &lt; entities &gt; and such?</p>&#x00D;&#x00A;</div>"));
-            var ce = new ContentEngine();
-            var frag = ce.RenderBlob(bl);
+            var ce = getContentEngine();
+            var frag = ce.RenderBlob(new CanonicalizedAbsolutePath("test"), new TreeID(), bl);
             output(frag);
         }
     }
