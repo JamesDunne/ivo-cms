2ec5f1d748901abb4182c61af4fcce1241e5e54c
James Dunne <james.jdunne@gmail.com>
2011-09-01 20:55:53 -0500

    Added beginnings of the XML semantic parser for blobs stored in IVO.

---
+++ IVO.CMS/ContentEngine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IVO.Definition.Models;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace IVO.CMS
+{
+    public sealed class ContentEngine
+    {
+        public HTMLFragment RenderBlob(Blob bl)
+        {
+            // Load the XML from the blob that is encoded as UTF-8:
+            XDocument xd = XDocument.Parse(Encoding.UTF8.GetString(bl.Contents));
+            // Create a string builder used to build the output polyglot HTML5 document fragment:
+            StringBuilder sb = new StringBuilder(bl.Contents.Length);
+
+            Stack<XNode> stk = new Stack<XNode>();
+            stk.Push(xd);
+            while (stk.Count > 0)
+            {
+                XNode n = stk.Pop();
+                XElement xe;
+
+                switch (n.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        xe = (XElement)n;
+                        sb.AppendFormat("<{0}", xe.Name);
+                        if (xe.HasAttributes)
+                        {
+                            sb.Append(String.Join(" ", from at in xe.Attributes() select at.ToString()));
+                        }
+                        if (xe.IsEmpty)
+                        {
+                            sb.Append(" />");
+                            break;
+                        }
+                        break;
+                    case XmlNodeType.EndElement:
+                        xe = (XElement)n;
+                        sb.AppendFormat("</{0}>", xe.Name);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            string result = sb.ToString();
+            return new HTMLFragment(result);
+        }
+    }
+}
---
+++ IVO.CMS/HTMLFragment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IVO.CMS
+{
+    public sealed class HTMLFragment
+    {
+        private string _fragment;
+
+        public HTMLFragment(string fragment)
+        {
+            _fragment = fragment;
+        }
+
+        public static explicit operator string(HTMLFragment fragment) { return fragment._fragment; }
+        public static implicit operator HTMLFragment(string fragment) { return new HTMLFragment(fragment); }
+    }
+}
