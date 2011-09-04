6268a4864b89645dc84797a9026c814cbb750ed0
James Dunne <james.jdunne@gmail.com>
2011-09-04 02:19:35 -0500

    More documentation on the interface.

--- IVO.CMS/Providers/CustomElements/ICustomElementProvider.cs
+++ IVO.CMS/Providers/CustomElements/ICustomElementProvider.cs
@@ -11,10 +11,18 @@ namespace IVO.CMS.Providers.CustomElements
     public interface ICustomElementProvider
     {
         /// <summary>
-        /// Gets the next provider in the chain or null.
+        /// Gets the next provider in the chain or null to signal the end of the chain.
         /// </summary>
         ICustomElementProvider Next { get; }
 
+        /// <summary>
+        /// Handles a custom "cms-" element in the context of the <paramref name="state"/>.
+        /// </summary>
+        /// <param name="elementName">The name of the element, beginning with "cms-".</param>
+        /// <param name="state">The current rendering state.</param>
+        /// <returns>true to mark element as processed; false to continue to next provider in the chain.
+        /// If the last provider in the chain returns false, the custom element is skipped entirely and
+        /// ignored.</returns>
         bool ProcessCustomElement(string elementName, RenderState state);
     }
 }
