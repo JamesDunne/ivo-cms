298f5c6a67d5621d4413f7563cba12e910ebaf70
James Dunne <james.jdunne@gmail.com>
2011-09-15 18:35:50 -0500

    New TreeController. Need to hack up some JSON model binders.

---
+++ IVO.CMS.API/Controllers/TreeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using IVO.CMS.API.Code;
+using IVO.CMS.Web.Mvc;
+using IVO.Definition.Models;
+using IVO.Definition.Containers;
+
+namespace IVO.CMS.API.Controllers
+{
+    public class TreeController : TaskAsyncController
+    {
+        private CMSContext cms;
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            this.cms = new CMSContext(new DirectoryInfo(Server.MapPath("~/ivo/")));
+
+            base.OnActionExecuting(filterContext);
+        }
+
+        [HttpGet]
+        [ActionName("get")]
+        public async Task<ActionResult> GetTreeByID(TreeID id)
+        {
+            var tr = await cms.trrepo.GetTree(id);
+
+            return Json(new { tr }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [ActionName("create")]
+        public async Task<ActionResult> CreateTree(TreeModel tm)
+        {
+            // TODO: model-binding for TreeModel.
+            var tr = await cms.trrepo.PersistTree(tm.root, tm.trees);
+
+            return Json(new { tr });
+        }
+    }
+}
---
+++ IVO.CMS.API/Controllers/TreeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IVO.Definition.Models;
+using IVO.Definition.Containers;
+
+namespace IVO.CMS.API.Controllers
+{
+    public sealed class TreeModel
+    {
+        // TODO: model binding FROM JSON.
+        public TreeID root;
+        public ImmutableContainer<TreeID, Tree> trees;
+    }
+}
