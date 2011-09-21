7d5bb8b7f32f0428fc6cbb7320623717e6a7a06c
James Dunne <james.jdunne@gmail.com>
2011-09-20 21:41:46 -0500

    Added basic MVC request models for tags/query.

--- IVO.CMS.API/Controllers/TagController.cs
+++ IVO.CMS.API/Controllers/TagController.cs
@@ -7,6 +7,7 @@ using IVO.CMS.API.Code;
 using IVO.CMS.Web.Mvc;
 using IVO.Definition.Models;
 using IVO.CMS.API.Models;
+using System.Collections.ObjectModel;
 
 namespace IVO.CMS.API.Controllers
 {
@@ -43,6 +44,69 @@ namespace IVO.CMS.API.Controllers
             var tg = await cms.tgrepo.GetTagByName(tagName);
 
             return Json(new { tag = tg.ToJSON() }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        [ActionName("query")]
+        public async Task<ActionResult> Query(TagQueryModel qm)
+        {
+            if (qm == null) qm = new TagQueryModel();
+
+            // Convert the simple filtering criteria:
+            TagQuery tq = new TagQuery(qm.dateFrom, qm.dateTo, qm.name, qm.tagger);
+
+            // Is ordering desired? Possibly paging?
+            if (qm.ordering != null)
+            {
+                // Convert the JSON model of ordering:
+                var orderBy = new ReadOnlyCollection<OrderByApplication<TagOrderBy>>(
+                    qm.ordering.SelectAsArray(ob => new OrderByApplication<TagOrderBy>(convertTagOrderBy(ob.by), convertDirection(ob.dir)))
+                );
+
+                // Determine if paging is requested and is valid:
+                if (qm.pageSize.HasValue && qm.pageNumber.HasValue && qm.pageSize.Value > 0 && qm.pageNumber > 0)
+                {
+                    // Paging looks valid:
+                    var results = await cms.tgrepo.SearchTags(tq, orderBy, new PagingRequest(qm.pageNumber.Value, qm.pageSize.Value));
+
+                    return Json(new { results = results }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    // No paging or invalid paging parameters:
+                    var results = await cms.tgrepo.SearchTags(tq, orderBy);
+
+                    return Json(new { results = results }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+            {
+                // No ordering or paging:
+                var results = await cms.tgrepo.SearchTags(tq);
+
+                return Json(new { results = results }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        private Definition.Models.OrderByDirection convertDirection(Models.OrderByDirModel orderByDirection)
+        {
+            switch (orderByDirection)
+            {
+                case Models.OrderByDirModel.asc: return Definition.Models.OrderByDirection.Ascending;
+                case Models.OrderByDirModel.desc: return Definition.Models.OrderByDirection.Descending;
+                default: return Definition.Models.OrderByDirection.Ascending;
+            }
+        }
+
+        private TagOrderBy convertTagOrderBy(TagQueryModel.OrderBy ob)
+        {
+            switch (ob)
+            {
+                case TagQueryModel.OrderBy.date_tagged: return TagOrderBy.DateTagged;
+                case TagQueryModel.OrderBy.name: return TagOrderBy.Name;
+                case TagQueryModel.OrderBy.tagger: return TagOrderBy.Tagger;
+                default: return TagOrderBy.DateTagged;
+            }
         }
 
         [HttpPost]
---
+++ IVO.CMS.API/Models/OrderByDirModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IVO.CMS.API.Models
+{
+    public enum OrderByDirModel
+    {
+        asc,
+        desc
+    }
+}
