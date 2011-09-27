7f7ae070ab84d17f2bba1178a141910c2abbf0f8
James Dunne <james.jdunne@gmail.com>
2011-09-26 21:25:00 -0500

    Load testing site. Unless I did something wrong, 60k req/sec is impressive.

--- PerformanceTestHost/Program.cs
+++ PerformanceTestHost/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 using TestCMS;
+using System.Diagnostics;
 
 namespace PerformanceTestHost
 {
@@ -10,7 +13,41 @@ namespace PerformanceTestHost
     {
         static void Main(string[] args)
         {
-            new FileSystemTests().SpeedTestRenderBlob();
+            new Program().Requests().Wait();
+        }
+
+        private async Task Requests()
+        {
+            const int per = 4;
+
+            HttpWebRequest[] rq = new HttpWebRequest[per];
+            Task<WebResponse>[] rspt = new Task<WebResponse>[per];
+            
+            for (int i = 0; i < per; ++i)
+            {
+                rq[i] = (HttpWebRequest)HttpWebRequest.Create("http://localhost:52729/commit/tree/ref/HEAD");
+                rq[i].Accept = "application/json";
+                rq[i].Method = "GET";
+                rq[i].Pipelined = false;
+            }
+
+            const int count = 8192;
+            
+            Stopwatch sw = Stopwatch.StartNew();
+            for (int i = 0; i < count; ++i)
+            {
+                for (int j = 0; j < per; ++j)
+                {
+                    rspt[j] = rq[j].GetResponseAsync();
+                }
+                
+                var rsp = await TaskEx.WhenAll(rspt);
+                for (int j = 0; j < per; ++j)
+                    rsp[j].Close();
+            }
+            sw.Stop();
+
+            Console.WriteLine("Total {0} requests in {1} msec = {2} req/sec", count * per, sw.ElapsedMilliseconds, (count * per) / (sw.ElapsedMilliseconds / 1000d));
         }
     }
 }
